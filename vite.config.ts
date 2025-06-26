ee414a847ed11b738a0c60dd22632c6ad0a3443b
Siddesh0002T <94625517+Siddesh0002T@users.noreply.github.com>
2025-06-26 10:00:33 +0530

    Add allowedHosts to Vite server config
    Configured the Vite dev server to allow connections from 'localhost' and a specific ngrok domain by setting the allowedHosts option. This enables development access from external tunnels.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(), react()],
+  plugins: [tailwindcss(), react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'aed4-2409-4081-1212-a8f0-edd2-6a93-e97c-e416.ngrok-free.app'
+    ]
+  }
 })
