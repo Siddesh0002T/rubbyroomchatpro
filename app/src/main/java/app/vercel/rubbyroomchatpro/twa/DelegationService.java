6772e8674cc0694533d9f13e32ca8786241faf1c
Siddesh0002T <94625517+Siddesh0002T@users.noreply.github.com>
2025-06-28 13:29:38 +0530

    tp

---
+++ app/src/main/java/app/vercel/rubbyroomchatpro/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.rubbyroomchatpro.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rubbysoft.rubbyroomchatpro',
-  appName: 'Rubby Room Chat Plus',
+  appId: 'com.example.app',
+  appName: 'rubbyroomchatpro',
   webDir: 'dist'
 };
 
